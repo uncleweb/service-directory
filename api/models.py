6b2f584b78df035b2323894c35b034237b62ceb1
Malte Meister <malte@informationlogistics.co.za>
2016-01-07 17:53:42 +0200

    First stab at models

--- api/models.py
+++ api/models.py
@@ -2,4 +2,53 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    iso_code = models.CharField(max_length=3)
+
+
+class CountryArea(models.Model):
+    AREA_LEVELS = [
+        (1, 'Province/State'),
+        (2, 'Region'),
+        (3, 'Municipality/City/Town'),
+        (4, 'Suburb/Area')
+    ]
+
+    name = models.CharField(max_length=100)
+    level = models.IntegerField(choices=AREA_LEVELS)
+    country = models.ForeignKey(Country)
+
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=100)
+    about = models.CharField(max_length=500)
+
+    address = models.CharField(max_length=500)
+    telephone = models.CharField(max_length=50)
+    email = models.EmailField()
+    web = models.URLField()
+
+    country = models.ForeignKey(Country)
+    areas = models.ManyToManyField(CountryArea)
+
+    # TODO: lat/lon, consider GeoDjango
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Service(models.Model):
+    categories = models.ManyToManyField(Category)
+    keywords = models.CharField(max_length=500)
+
+    organisation = models.ForeignKey(Organisation)
+
+    verified_as = models.CharField(max_length=100)
+
+    age_range_min = models.PositiveSmallIntegerField()
+    age_range_max = models.PositiveSmallIntegerField()
+
+    availability_hours = models.CharField(max_length=50)  # might want separate min & max fields / DateTimeField or DurationField
