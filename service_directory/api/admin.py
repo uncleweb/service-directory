1d1a1983baa6a924a61c6fa6583043361c9cb009
Malte Meister <malte@informationlogistics.co.za>
2016-01-15 10:26:58 +0200

    SED-5: turn on OpenStreetMaps for the Organisation model

--- service_directory/api/admin.py
+++ service_directory/api/admin.py
@@ -1,9 +1,10 @@
 from models import Country, CountryArea, Organisation, Category, Service
-from django.contrib import admin
+from django.contrib.gis import admin
+
 
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(CountryArea)
-admin.site.register(Organisation)
+admin.site.register(Organisation, admin.OSMGeoAdmin)
 admin.site.register(Category)
 admin.site.register(Service)
