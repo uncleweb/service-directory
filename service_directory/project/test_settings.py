73087a589239d40d598348f4ebf02955ceafb79f
Malte Meister <malte@informationlogistics.co.za>
2016-02-01 11:31:04 +0200

    doc

--- service_directory/project/test_settings.py
+++ service_directory/project/test_settings.py
@@ -2,7 +2,7 @@ from settings import *
 
 
 # we need a user that is a superuser because we need to be able to run
-# CREATE EXTENSION postgis;
+# CREATE EXTENSION commands
 # CREATEDB permission is also required
 DATABASES['default']['USER'] = 'postgres'
 DATABASES['default']['PASSWORD'] = 'password'
