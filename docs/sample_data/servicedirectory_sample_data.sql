ddeb0fdedb2efade20bdfc04630cf8a02588dd76
Malte Meister <malte@informationlogistics.co.za>
2016-04-05 17:36:36 +0200

    SED-44, SED-46: update sample data script for data model changes

--- docs/sample_data/servicedirectory_sample_data.sql
+++ docs/sample_data/servicedirectory_sample_data.sql
@@ -22,24 +22,24 @@ INSERT INTO "api_keyword" (name,show_on_home_page) VALUES('Test',True) RETURNING
 INSERT INTO "api_keyword" (name,show_on_home_page) VALUES('HIV',True) RETURNING id INTO hiv_keyword_id;
 INSERT INTO "api_keyword" (name,show_on_home_page) VALUES('Tutoring',True) RETURNING id INTO tutoring_keyword_id;
 
---keyword_categories
-INSERT INTO "api_keyword_categories" (keyword_id,category_id) VALUES(test_keyword_id,health_category_id);
-INSERT INTO "api_keyword_categories" (keyword_id,category_id) VALUES(test_keyword_id,education_category_id);
-INSERT INTO "api_keyword_categories" (keyword_id,category_id) VALUES(hiv_keyword_id,health_category_id);
-INSERT INTO "api_keyword_categories" (keyword_id,category_id) VALUES(tutoring_keyword_id,education_category_id);
+--keyword_category
+INSERT INTO "api_keywordcategory" (keyword_id,category_id) VALUES(test_keyword_id,health_category_id);
+INSERT INTO "api_keywordcategory" (keyword_id,category_id) VALUES(test_keyword_id,education_category_id);
+INSERT INTO "api_keywordcategory" (keyword_id,category_id) VALUES(hiv_keyword_id,health_category_id);
+INSERT INTO "api_keywordcategory" (keyword_id,category_id) VALUES(tutoring_keyword_id,education_category_id);
 
 --country
 INSERT INTO "api_country" (name,iso_code) VALUES('South Africa','ZA') RETURNING id INTO sa_country_id;
 
 --organisation
-INSERT INTO "api_organisation" (name,about,address,telephone,email,web,verified_as,opening_hours,country_id,location) VALUES('Healthcare Co','Something about them','202 The Gatehouse, Century Way, Century City','0215522159','blueteam@labs.ws','http://www.afrolabs.co.za','','',sa_country_id,'SRID=4326;POINT (18.5054960000000008 -33.8919369999999986)') RETURNING id INTO healthcareco_org_id;
+INSERT INTO "api_organisation" (name,about,address,telephone,emergency_telephone,email,web,verified_as,opening_hours,country_id,location,facility_code) VALUES('Healthcare Co','Something about them','202 The Gatehouse, Century Way, Century City','0215522159','','blueteam@labs.ws','http://www.afrolabs.co.za','','',sa_country_id,'SRID=4326;POINT (18.5054960000000008 -33.8919369999999986)','') RETURNING id INTO healthcareco_org_id;
 
---organisation_categories
-INSERT INTO "api_organisation_categories" (organisation_id,category_id) VALUES(healthcareco_org_id,health_category_id);
-INSERT INTO "api_organisation_categories" (organisation_id,category_id) VALUES(healthcareco_org_id,education_category_id);
+--organisation_category
+INSERT INTO "api_organisationcategory" (organisation_id,category_id) VALUES(healthcareco_org_id,health_category_id);
+INSERT INTO "api_organisationcategory" (organisation_id,category_id) VALUES(healthcareco_org_id,education_category_id);
 
---organisation_keywords
-INSERT INTO "api_organisation_keywords" (organisation_id,keyword_id) VALUES(healthcareco_org_id,test_keyword_id);
-INSERT INTO "api_organisation_keywords" (organisation_id,keyword_id) VALUES(healthcareco_org_id,hiv_keyword_id);
+--organisation_keyword
+INSERT INTO "api_organisationkeyword" (organisation_id,keyword_id) VALUES(healthcareco_org_id,test_keyword_id);
+INSERT INTO "api_organisationkeyword" (organisation_id,keyword_id) VALUES(healthcareco_org_id,hiv_keyword_id);
 
 END $$;
