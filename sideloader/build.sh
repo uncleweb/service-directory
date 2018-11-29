251198317c00da76baeb32bd9b1221a55ab4da20
Saeed Marzban <Saeed.mb8@gmail.com>
2018-11-29 13:45:35 +0200

    remove docker and sideloader folder

--- sideloader/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $REPO
-
-docker build -t service-directory .
-
-docker tag -f service-directory qa-mesos-persistence.za.prk-host.net:5000/service-directory
-
-docker push qa-mesos-persistence.za.prk-host.net:5000/service-directory
